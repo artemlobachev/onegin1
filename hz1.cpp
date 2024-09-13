a33c2d19660b86c46e87664de9dffc641da66d0b
artemlobachev <lob_art@mail.ru>
2024-09-13 12:26:50 +0300

    ver1

--- hz1.cpp
+++ hz1.cpp
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <ctype.h>
 
 void BubbleSortForStrings(char *SomeText, const int strings, const int strlen, int *index);
-void swap(void *a, void *b);
+void swap(void *a, void *b, size_t MemSize);
 int *FromZeroToN(int *index, int n);
+int strcmpAnyRegist(char *word1, char *word2);
 
 int main()
 {
     //best way dynamical memory w variable len string
     const int STRINGS = 4;
     const int STRLEN = 32;
-    char SomeText[STRINGS][STRLEN] = {"alfred envelope" ,"alfred aoggins",
-                                            "alfred coogings","alfred bwens"};
+    char SomeText[STRINGS][STRLEN] = {"alfred envelope" ,"alfred Aoggins",
+                                            "alfred coDSAsgings","alfred DSBwens"};
 
     int index[STRINGS] = {};
     FromZeroToN(index, STRINGS);
@@ -30,17 +32,17 @@ void BubbleSortForStrings(char *SomeText, const int strings, const int strlen, i
     for( int step = 0; step < strings; step++)
         for (int i = step; i < strings - step - 1; i++)
         {
-            const char *FirstStr = SomeText + strlen * index[i];
-            const char *SecondStr = SomeText + strlen * index[i+1];
+            char *FirstStr = SomeText + strlen * index[i];
+            char *SecondStr = SomeText + strlen * index[i+1];
 
             assert(FirstStr != NULL && SecondStr != NULL);
-            //UpToLow(FirstStr, SecondStr);
+            assert(FirstStr != SecondStr);
 
-            if (strcmp(FirstStr, SecondStr) > 0)
+            if (strcmpAnyRegist(FirstStr, SecondStr) > 0)
             {
                 assert(&index[i] != NULL && &index[i+1] != NULL);
 
-                swap(&index[i], &index[i+1]);
+                swap(&index[i], &index[i+1], sizeof(&index[i]));
             }
         }
 }
@@ -53,13 +55,47 @@ int *FromZeroToN(int *index, int n)
     return index;
 }
 
-//void UpToLow(const char
 
-void swap(void *a, void *b)
+
+void swap(void *a, void *b, size_t MemSize)
 {
     assert(a != NULL && b != NULL);
+    assert(a != b);
 
-    void c = b;
-    *a = *b;
-    *b = c;
+    size_t BufferSize = 33;
+    char buffer[BufferSize];
+
+    for (size_t i = 0; MemSize > BufferSize; i++)
+    {
+        MemSize-= BufferSize;
+        memcpy(buffer, a, BufferSize);
+        memcpy(a, b, BufferSize);
+        memcpy(b, buffer, BufferSize);
+
+        a += BufferSize;
+        b += BufferSize;
+    }
+
+    memcpy(buffer, a, MemSize);
+    memcpy(a, b, MemSize);
+    memcpy(b, buffer, MemSize);
+}
+
+int strcmpAnyRegist(char *word1, char *word2)
+{
+    assert(word1 != NULL && word2 != NULL);
+    assert(word1 != word2);
+
+    while (*word1++ != '\0' && *word2++ != '\0')
+    {
+        char charcmp1 = tolower(*word1);
+        char charcmp2 = tolower(*word2);
+
+        if (charcmp1 > charcmp2)
+            return 1;
+        if (charcmp1 < charcmp2)
+            return -1;
+    }
+
+    return 0;
 }
